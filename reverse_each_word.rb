d02b266a09d5d47bdad8f3b79de66aaa4f65ad27
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-08 23:57:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence = ["Hello there, and how are you?"]
 def reverse_each_word(sentence)
   new_sentence = sentence.split("")
-  reversed_sentence = new_sentence
+  reversed_sentence = new_sentence.each do ||
   
   puts sentence.reverse 
 end
