398004c434b58446bb05f682548589d3e1101c07
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-09 00:11:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,6 +5,6 @@ def reverse_each_word(sentence)
 end
 def reverse_each_word(sentence)
   new_sentence = sentence.split("")
-  reversed_sentence = new_sentence.collect {|x| x.reverse!}
+  new_sentence.collect {|x| x.reverse!}
   return reversed_sentence.join("")
 end
