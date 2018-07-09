3c3d5d62508b359df205fe9d3c017e62e9b5da5e
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-09 00:10:43 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,4 +4,7 @@ def reverse_each_word(sentence)
   return reversed_sentence.join(" ")
 end
 def reverse_each_word(sentence)
-  
+  new_sentence = sentence.split("")
+  reversed_sentence = new_sentence.collect {|x| x.reverse!}
+  return reversed_sentence.join("")
+end
