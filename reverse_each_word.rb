91d6d2625295fbeeaa3c6b59a81ba0c035780fc4
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-09 00:01:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
-
-def reverse_each_word(sentence)
-  new_sentence = sentence.split("")
-  reversed_sentence = new_sentence.each {|x| x.reverse!}
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
   return reversed_array.join(" ")
 end
